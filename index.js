2760777690c89d828169648ef274f0acb8c21f03
YueHuaHua <bulanarum@gmail.com>
2023-02-24 18:56:34 +0000

    added writeToFile function

--- index.js
+++ index.js
@@ -10,11 +10,16 @@ const questions = [
 
 // function to write README file
 function writeToFile(fileName, data) {
+    inquirer.prompt(questions).then((data) => {
+        // writes to a file in the folder path, also has handling for errors
+        fs.writeFile(path + "/" + fileName, formatData(data), (err) => err ? console.log(err) : console.log('success'))
+    })
 }
 
 // function to initialize program
 function init() {
-
+    inquirer.prompt(questions);
+    writeToFile();
 }
 
 // function call to initialize program
